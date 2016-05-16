40697f09fe01b9a54ec9a4f7d6cfea6e1e4a41f2
rails --help <guraybaydur@gmail.com>
2016-05-16 22:09:13 +0200

    Database connection fixed

---
+++ MySqlDatabaseInterfaces/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using DomainModel;
+using System.Collections.Generic;
+
+namespace DataAccessLayer
+{
+    public interface IRepository
+    {
+
+        List<Comment> GetCommentsOfAPost(int postId, int limit, int offset);
+        int GetNumberOfCommentsOfAPost(int postId);
+
+        List<History> GetAllHistory(int limit, int offset);
+        int GetNumberOfHistories();
+        History GetAHistory(int? id);
+        void AddToHistory(History history);
+        void DeleteFromHistory(int? id);
+        void DeleteAllHistory();
+
+        User GetUserById(int id);
+
+        List<Mark> GetMarkedPosts(int limit, int offset);
+        int GetNumberOfMarkedPosts();
+        void Mark(Mark mark);
+        void Unmark(int id);
+        void DeleteAllMarks();
+
+        Post GetAPost(int postId);
+        List<Post> GetAllRelatedPosts(int postId);
+
+        WordIdf GetIDFOfWord(string word);
+        List<WordTf> GetTFsOfWord(int wordId);
+    }
+}
